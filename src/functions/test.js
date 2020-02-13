e22b6c5ed540d4b1c2b839f2c3770e6d0e0564b4
Chris Borbidge <chris@elkthelabel.com>
2020-02-13 17:00:29 +1100

    Update test.js

--- src/functions/test.js
+++ src/functions/test.js
@@ -4,48 +4,44 @@ const querystring = require("querystring")
 
 exports.handler = async (event, context) => {
   const params = event.queryStringParameters
-  const shop = event.queryStringParameters.shop
+  const { shop, path_prefix, timestamp } = event.queryStringParameters
   const apiSecret = process.env.SHOPIFY_API_SECRET
 
-  const { signature } = params.query;
-  const map = Object.assign({}, req.query);
-  delete map.signature;
-  let message = Object.keys(map).map(function (i) {
-    return `${i}=${map[i]}`;
-  }).sort().join('');
-  const providedSignature = Buffer.from(signature, 'utf-8');
-  const generatedHash = Buffer.from(
-    crypto
-      .createHmac('sha256', apiSecret)
-      .update(message)
-      .digest('hex'),
-    'utf-8'
-  );
-  let hashEquals = false;
-  console.log('generatedHash', generatedHash);
-  console.log('providedSignature', providedSignature);
-  // timingSafeEqual will prevent any timing attacks. Arguments must be buffers
-  try {
-    hashEquals = crypto.timingSafeEqual(generatedHash, providedSignature);
-    // timingSafeEqual will return an error if the input buffers are not the same length.
-  } catch (e) {
-    hashEquals = false;
-  };
+  if (shop && path_prefix && timestamp) {
+    const map = { path_prefix, shop, timestamp }
+    const message = querystring.stringify(map)
+    const providedHmac = Buffer.from(signature, 'utf-8')
+    const generatedHash = Buffer.from(
+      crypto
+        .createHmac('sha256', apiSecret)
+        .update(message)
+        .digest('hex'),
+      'utf-8'
+    )
 
-  if (!hashEquals) {
-    return {
-      statusCode: 400,
-      body: "HMAC validation failed"
+    let hashEquals = false
+
+    try {
+      hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+    } catch (e) {
+      hashEquals = false
+    };
+
+    if (!hashEquals) {
+      return {
+        statusCode: 400,
+        body: "HMAC validation failed"
+      }
     }
-  }
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      shop: shop
-    })
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        params: params
+      })
+    }
   }
 }
