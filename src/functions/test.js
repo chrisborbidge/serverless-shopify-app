117f9dec57d15b5dcb9d0ae3a89453e6c701cbf6
Chris Borbidge <chris@elkthelabel.com>
2020-02-13 13:55:21 +1100

    Update test.js

--- src/functions/test.js
+++ src/functions/test.js
@@ -1,13 +1,41 @@
+const crypto = require("crypto")
 exports.handler = async (event, context) => {
   const params = event.queryStringParameters
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      params: params
-    })
+  if (shop && path_prefix && timestamp) {
+    const map = { shop, path_prefix, timestamp }
+    const message = querystring.stringify(map)
+    const providedHmac = Buffer.from(signature, 'utf-8')
+    const generatedHash = Buffer.from(
+      crypto
+        .createHmac('sha256', apiSecret)
+        .update(message)
+        .digest('hex'),
+      'utf-8'
+    )
+
+    let hashEquals = false
+
+    try {
+      hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+    } catch (e) {
+      hashEquals = false
+    };
+
+    if (!hashEquals) {
+      return {
+        statusCode: 400,
+        body: "HMAC validation failed"
+      }
+    }
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        params: params
+      })
+    }
   }
-}
