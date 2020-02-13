402b9bdfd1371b2a52057d8891985b201430c8f2
Chris Borbidge <chris@elkthelabel.com>
2020-02-13 14:32:13 +1100

    Update test.js

--- src/functions/test.js
+++ src/functions/test.js
@@ -6,8 +6,8 @@ exports.handler = async (event, context) => {
   const { shop, path_prefix, timestamp, signature } = event.queryStringParameters
   const apiSecret = process.env.SHOPIFY_API_SECRET
 
-  if (shop && path_prefix && timestamp) {
-    const map = { path_prefix, shop, timestamp }
+  if (shop && path_prefix && state && timestamp) {
+    const map = { path_prefix, shop, state, timestamp }
     const message = querystring.stringify(map)
     const providedHmac = Buffer.from(signature, 'utf-8')
     const generatedHash = Buffer.from(
